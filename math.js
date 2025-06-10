651a9a321c2457384575be7f7ec452b13fb1ee3d
sambhavhere17 <sambhavjain3292@gmail.com>
2025-06-10 20:06:30 +0530

    fruits

---
+++ math.js
@@ -0,0 +1,7 @@
+let obj = {
+  sum: (a, b) => a + b,
+  mul: (a, b) => a * b,
+  g: 9.8,
+  PI: 3.14,
+};
+module.exports = obj;
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const maths = require("./math");
+
 console.log(process.argv);
+
+console.log(maths);
+console.log(maths.PI);
+maths;
+console.log(maths.mul(2, 6));
