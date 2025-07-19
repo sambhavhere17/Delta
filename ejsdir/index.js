bcc21f89a25f8b9a6c11c955dcc9f9d65e32a58b
sambhavhere17 <sambhavjain3292@gmail.com>
2025-07-19 20:40:29 +0530

    add

--- ejsdir/index.js
+++ ejsdir/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
